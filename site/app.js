e6349af88f15574ccd11250e360badf95f9f298b
solOcampo <soolangeeliizabet@gmail.com>
2022-07-09 06:52:19 -0300

    Estructura MVC terminada

--- site/app.js
+++ site/app.js
@@ -1,8 +1,19 @@
+
 const express=require('express')
 const app=express()
-const path=require('path')
 const port=3210
+const path=require('path')
+
 
 app.use(express.static(path.resolve(__dirname,'public')))
 
-app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'views','home.html')))
+
+app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/detalle',(req,res)=> res.sendFile(path.join(__dirname,'views','detalle.html')))
+app.get('/carrito',(req,res)=> res.sendFile(path.join(__dirname,'views','carrito.html')))
+app.get('/login',(req,res)=> res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/register',(req,res)=> res.sendFile(path.join(__dirname,'views','register.html')))
+
+
+app.listen(port,() => console.log(`El servidor fue levantado con exito en el puerto ${port}`))
+
