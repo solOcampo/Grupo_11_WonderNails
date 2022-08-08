fe95479fc59d5e48725c527e89f6d3e864ed8f76
solOcampo <soolangeeliizabet@gmail.com>
2022-08-08 19:28:01 -0300

    Agrego vista admin

---
+++ site/controllers/adminController.js
@@ -0,0 +1,24 @@
+let productos = require('../data/productos.json')
+
+
+module.exports = {
+    list: (req,res) => {
+        return res.render('admin/listarProductos',{
+            productos
+        })
+    },
+    create:(req,res) => {
+        return res.render('admin/crearProducto')
+    },
+    edit:(req,res) => {
+        return res.render('admin/editarProducto')
+        id = +req.params.id
+        let producto = productos.find((elemento) => {
+            return elemento.id == id
+        })
+        /* return res.send(producto) Comprobar que esta llegando bien el elemento*/
+        return res.render('admin/editarProducto',{
+            producto
+        })
+    },
+}
