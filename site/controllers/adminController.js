3a1d0798a2e4c2bd1aba6ffd0866df13adfe6feb
MicaSLoureiro <micaela_loureiro@hotmail.com>
2022-08-25 19:14:19 -0300

    Subo cambios en html de crear

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -22,6 +22,7 @@ module.exports = {
 			marca : req.body.marca,
 			categoria : req.body.categoria,
 			estado : req.body.estado,
+            color: req.body.color,
 			precio : req.body.precio,
 			descuento : req.body.descuento,
 			stock : req.body.stock,
