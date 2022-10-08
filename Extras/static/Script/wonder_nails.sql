67443f4aea2cce5dac135a1d1130ef791412bcd7
solOcampo <soolangeeliizabet@gmail.com>
2022-10-08 14:43:49 -0300

    Modifico archivos en la carpeta script

--- Extras/static/Script/wonder_nails.sql
+++ Extras/static/Script/wonder_nails.sql
@@ -5,27 +5,75 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema 
+-- Schema wonder_nails
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema 
+-- Schema wonder_nails
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `` DEFAULT CHARACTER SET utf8 ;
-USE `` ;
+CREATE SCHEMA IF NOT EXISTS `wonder_nails` DEFAULT CHARACTER SET utf8 ;
+USE `wonder_nails` ;
 
 -- -----------------------------------------------------
--- Table ``.`Usuarios`
+-- Table `wonder_nails`.`Categorias`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ``.`Usuarios` (
+CREATE TABLE IF NOT EXISTS `wonder_nails`.`Categorias` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `wonder_nails`.`Marcas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `wonder_nails`.`Marcas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `wonder_nails`.`Productos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `wonder_nails`.`Productos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `stock` INT NOT NULL,
+  `precio` INT NOT NULL,
+  `descripcion` VARCHAR(500) NOT NULL,
+  `descuento` INT NOT NULL,
+  `Categorias_id` INT NOT NULL,
+  `Marcas_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Productos_Categorias1_idx` (`Categorias_id` ASC) VISIBLE,
+  INDEX `fk_Productos_Marcas1_idx` (`Marcas_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Productos_Categorias1`
+    FOREIGN KEY (`Categorias_id`)
+    REFERENCES `wonder_nails`.`Categorias` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Productos_Marcas1`
+    FOREIGN KEY (`Marcas_id`)
+    REFERENCES `wonder_nails`.`Marcas` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `wonder_nails`.`Usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `wonder_nails`.`Usuarios` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellido` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `telefono` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  `cuidad` VARCHAR(45) NOT NULL,
-  `genero` VARCHAR(45) NOT NULL,
   `codigoPostal` INT NOT NULL,
+  `ciudad` VARCHAR(45) NOT NULL,
+  `genero` VARCHAR(45) NOT NULL,
   `imagen` VARCHAR(45) NOT NULL,
   `rol` VARCHAR(45) NOT NULL,
   `contraseña` VARCHAR(100) NOT NULL,
@@ -34,75 +82,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table ``.`Categorias`
+-- Table `wonder_nails`.`Carritos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ``.`Categorias` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table ``.`Marcas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ``.`Marcas` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table ``.`Productos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ``.`Productos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `stock` INT NOT NULL,
-  `precio` INT NOT NULL,
-  `descuento` INT NOT NULL,
-  `descripcion` VARCHAR(500) NOT NULL,
-  `Marcas_id` INT NOT NULL,
-  `Categorias_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Productos_Marcas_idx` (`Marcas_id` ASC) VISIBLE,
-  INDEX `fk_Productos_Categorias1_idx` (`Categorias_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Productos_Marcas`
-    FOREIGN KEY (`Marcas_id`)
-    REFERENCES ``.`Marcas` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Productos_Categorias1`
-    FOREIGN KEY (`Categorias_id`)
-    REFERENCES ``.`Categorias` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table ``.`Imagenes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ``.`Imagenes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `id_producto` INT NOT NULL,
-  `Productos_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Imagenes_Productos1_idx` (`Productos_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Imagenes_Productos1`
-    FOREIGN KEY (`Productos_id`)
-    REFERENCES ``.`Productos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table ``.`Carritos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ``.`Carritos` (
+CREATE TABLE IF NOT EXISTS `wonder_nails`.`Carritos` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `Productos_id` INT NOT NULL,
   `Usuarios_id` INT NOT NULL,
@@ -111,35 +93,52 @@ CREATE TABLE IF NOT EXISTS ``.`Carritos` (
   INDEX `fk_Carritos_Usuarios1_idx` (`Usuarios_id` ASC) VISIBLE,
   CONSTRAINT `fk_Carritos_Productos1`
     FOREIGN KEY (`Productos_id`)
-    REFERENCES ``.`Productos` (`id`)
+    REFERENCES `wonder_nails`.`Productos` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Carritos_Usuarios1`
     FOREIGN KEY (`Usuarios_id`)
-    REFERENCES ``.`Usuarios` (`id`)
+    REFERENCES `wonder_nails`.`Usuarios` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table ``.`Ordenes`
+-- Table `wonder_nails`.`Ordenes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ``.`Ordenes` (
+CREATE TABLE IF NOT EXISTS `wonder_nails`.`Ordenes` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `Usuarios_id` INT NOT NULL,
   `Carritos_id` INT NOT NULL,
+  `Usuarios_id` INT NOT NULL,
   PRIMARY KEY (`id`),
+  INDEX `fk_Ordenes_Carritos_idx` (`Carritos_id` ASC) VISIBLE,
   INDEX `fk_Ordenes_Usuarios1_idx` (`Usuarios_id` ASC) VISIBLE,
-  INDEX `fk_Ordenes_Carritos1_idx` (`Carritos_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Ordenes_Usuarios1`
-    FOREIGN KEY (`Usuarios_id`)
-    REFERENCES ``.`Usuarios` (`id`)
+  CONSTRAINT `fk_Ordenes_Carritos`
+    FOREIGN KEY (`Carritos_id`)
+    REFERENCES `wonder_nails`.`Carritos` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Ordenes_Carritos1`
-    FOREIGN KEY (`Carritos_id`)
-    REFERENCES ``.`Carritos` (`id`)
+  CONSTRAINT `fk_Ordenes_Usuarios1`
+    FOREIGN KEY (`Usuarios_id`)
+    REFERENCES `wonder_nails`.`Usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `wonder_nails`.`imagenes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `wonder_nails`.`imagenes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `Productos_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_imagenes_Productos1_idx` (`Productos_id` ASC) VISIBLE,
+  CONSTRAINT `fk_imagenes_Productos1`
+    FOREIGN KEY (`Productos_id`)
+    REFERENCES `wonder_nails`.`Productos` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
