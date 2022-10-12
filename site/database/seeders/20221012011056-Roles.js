c40d05d9b25226f23e4903b7edba3ac31f662797
MicaSLoureiro <micaela_loureiro@hotmail.com>
2022-10-11 23:37:38 -0300

    Subo seed en Roles

--- site/database/seeders/20221012011056-Roles.js
+++ site/database/seeders/20221012011056-Roles.js
@@ -1,25 +1,22 @@
 'use strict';
+let listado = ["Administrador", "Usuario"]
+let roles = listado.map(nombre => {
+  let elemento = {
+    rol:nombre,
+    createdAt: new Date,
+    updatedAt: new Date
+  }
+  return elemento
+})
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+ 
+     await queryInterface.bulkInsert('Roles', roles, {});
+   
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  await queryInterface.bulkDelete('Roles', null, {});  
   }
 };
